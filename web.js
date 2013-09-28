d2282754748118b036cf4d0c48e47c1ed6b5d0df
Palimar Rao <palimar@seas.upenn.edu>
2013-09-28 02:54:29 -0400

    No changes

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
- 
+
 app.get('/', function(req, res) {
-   res.sendfile('./views/index.html');
+	res.sendfile('./views/index.html');
 });
- 
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
 
